618a07e6f739931bc969f0275e7d211b7fd2f1f2
Greg Fiumara <gregory.fiumara@nist.gov>
2016-07-29 12:17:49 -0400

    Update imagery.

--- minexiii/validation/validation_imagery_raw/make_pairs.py
+++ minexiii/validation/validation_imagery_raw/make_pairs.py
@@ -11,40 +11,46 @@
 import random;
 import os;
 
-print "\tconst std::list<std::pair<std::string, std::string>> SAMPLE_PAIRS = {"
+print "\tconst std::vector<std::pair<std::string, std::string>> SAMPLE_PAIRS = {"
 
 # [subject : [finger : (encounter1, encounter2, ...)]]
 subjects = dict()
 
 for filename in os.listdir('.'):
 	try:
-		subject, encounter, finger = filename.replace(".raw", ".raw.tmpl").split('_')
+		if filename[-4:] != "gray":
+			continue
+
+		encounter_subject, finger = filename.replace(".gray", ".gray.tmpl").split('_')
+		encounter = encounter_subject[0]
+		subject = encounter_subject[1:]
 	except:
 		continue
 
 	# Skip "enrollment" template
-	if encounter == "def":
+	if encounter == "b":
 		continue
-		
+
 	if not subject in subjects:
 		subjects[subject] = dict()
 	if not finger in subjects[subject]:
 		subjects[subject][finger] = tuple()
 	subjects[subject][finger] += (encounter,)
-	
+
 # Start with genuine encounter -> enrollment
 for subject in subjects:
 	for finger in subjects[subject]:
 		for encounter in subjects[subject][finger]:
-			print "\t\t{{\"{subject}_{encounter}_{finger}\", \"{subject}_def_{finger}\"}},".format(subject = subject, encounter = encounter, finger = finger)
+			if encounter != "a" and encounter != "b":
+				continue
+			print "\t\t{{\"{encounter}{subject}_{finger}\", \"b{subject}_{finger}\"}},".format(subject = subject, encounter = encounter, finger = finger)
 
-# Add some genuine encounter -> encounter 
+# Add some genuine encounter -> encounter
 for subject in subjects:
 	for finger in subjects[subject]:
-		for i in range(0, 3):
-			encounter1 = random.choice(subjects[subject][finger])
-			encounter2 = random.choice(subjects[subject][finger])
-			print "\t\t{{\"{subject}_{encounter1}_{finger}\", \"{subject}_{encounter2}_{finger}\"}},".format(subject = subject, encounter1 = encounter1, encounter2 = encounter2, finger = finger)
+		encounter1 = random.choice(subjects[subject][finger])
+		encounter2 = random.choice(subjects[subject][finger])
+		print "\t\t{{\"{encounter1}{subject}_{finger}\", \"{encounter2}{subject}_{finger}\"}},".format(subject = subject, encounter1 = encounter1, encounter2 = encounter2, finger = finger)
 
 # Finish with impostors
 subject_ids = subjects.keys()
@@ -55,16 +61,16 @@ for i in range(0, max_impostor):
 	subject2 = random.choice(subject_ids)
 	if subject1 == subject2:
 		continue
-	
+
 	fingers = subjects[subject1].keys()
 	finger = random.choice(fingers)
 	if finger not in subjects[subject2]:
 		continue
-	
+
 	encounter1 = random.choice(subjects[subject1][finger])
 	encounter2 = random.choice(subjects[subject2][finger])
 
-	print "\t\t{{\"{subject1}_{encounter1}_{finger}\", \"{subject2}_{encounter2}_{finger}\"}}{comma}".format(subject1 = subject1, encounter1 = encounter1, subject2 = subject2, encounter2 = encounter2, finger = finger, comma = ("," if i != max_impostor - 1 else ""))
+	print "\t\t{{\"{encounter1}{subject1}_{finger}\", \"{encounter2}{subject2}_{finger}\"}}{comma}".format(subject1 = subject1, encounter1 = encounter1, subject2 = subject2, encounter2 = encounter2, finger = finger, comma = ("," if i != max_impostor - 1 else ""))
 	num_impostor += 1
 
 print "\t\t/* Num Impostor = {0} */".format(num_impostor)
